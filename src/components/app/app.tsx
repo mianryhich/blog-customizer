71e8d18ba689359620e84faad5ed558f87a2442e
mianryhich <mianettta@yandex.ru>
2026-06-19 11:43:29 +0300

    Добавлены все компоненты и реализоана логика применение параметров

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,12 +3,20 @@ import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	defaultArticleState,
+	ArticleStateType,
+} from './../../constants/articleProps';
 
 import styles from './app.module.scss';
 
 export const App = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const [appliedState, setAppliedState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	const sidebarRef = useRef<HTMLDivElement | null>(null);
 
 	const toggleOpen = () => setIsOpen(!isOpen);
@@ -37,15 +45,21 @@ export const App = () => {
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedState.fontFamilyOption.value,
+					'--font-size': appliedState.fontSizeOption.value,
+					'--font-color': appliedState.fontColor.value,
+					'--container-width': appliedState.contentWidth.value,
+					'--bg-color': appliedState.backgroundColor.value,
 				} as CSSProperties
 			}>
 			<div ref={sidebarRef}>
-				<ArticleParamsForm isOpen={isOpen} onClick={toggleOpen} />
+				<ArticleParamsForm
+					isOpen={isOpen}
+					onClick={toggleOpen}
+					formState={formState}
+					setFormState={setFormState}
+					onApplay={(newState: ArticleStateType) => setAppliedState(newState)}
+				/>
 			</div>
 			<Article />
 		</main>
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,16 +2,36 @@ import { useRef } from 'react';
 
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	fontFamilyOptions,
+	fontColors,
+	fontSizeOptions,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+} from 'src/constants/articleProps';
 
 export const ArticleParamsForm = ({
 	isOpen,
 	onClick,
+	formState,
+	setFormState,
+	onApplay,
 }: {
 	isOpen: boolean;
 	onClick: () => void;
+	formState: ArticleStateType;
+	setFormState: (state: ArticleStateType) => void;
+	onApplay: (state: ArticleStateType) => void;
 }) => {
 	const sidebarRef = useRef<HTMLDivElement>(null);
 
@@ -22,9 +42,67 @@ export const ArticleParamsForm = ({
 				ref={sidebarRef}
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Выбрать шрифт'
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={(option) =>
+							setFormState({ ...formState, fontFamilyOption: option })
+						}
+					/>
+					<RadioGroup
+						name='fontSize'
+						title='Размер шрифта'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={(option) =>
+							setFormState({ ...formState, fontSizeOption: option })
+						}
+					/>
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={formState.fontColor}
+						onChange={(option) =>
+							setFormState({ ...formState, fontColor: option })
+						}
+					/>
+					<Separator />
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						onChange={(option) =>
+							setFormState({ ...formState, backgroundColor: option })
+						}
+					/>
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={(option) =>
+							setFormState({ ...formState, contentWidth: option })
+						}
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={() => {
+								setFormState(defaultArticleState);
+								onApplay(defaultArticleState);
+							}}
+						/>
+						<Button
+							title='Применить'
+							htmlType='button'
+							type='apply'
+							onClick={() => onApplay(formState)}
+						/>
 					</div>
 				</form>
 			</aside>
